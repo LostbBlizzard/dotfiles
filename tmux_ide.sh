f17ab2fbbe33ce28bdcf3e6ca05090664d7d24e6
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2024-02-03 09:32:46 -0800

    updated tmux_ide

--- tmux_ide.sh
+++ tmux_ide.sh
@@ -5,10 +5,15 @@ cd "$WORKDIR"
 
 SESSION=$(basename "$INITDIR")
 
+
+
 tmux \
 	new-session -s "$SESSION" \; \
 	send-keys 'nnn -c '"$INITDIR"' -f -o -e' Enter \; \
-	split-window -c "$INITDIR" -h -p 85 hx \; \
+	split-window -c "$INITDIR" -h -p 80 hx \; \
 	send-keys 'hx' Enter \; \
 	send-keys " " "f" \; \
-	new-window -c "$INITDIR" -d \; \
+    split-window -c "$INITDIR" -h -p 15 sh \; \
+    split-window -c "$INITDIR" -v -p 40 lazygit \; \
+     
+ 
