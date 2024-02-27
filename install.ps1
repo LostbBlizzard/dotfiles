05c3ae8d76057325c9ed72fdc7e7dfc8b32622f9
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2024-02-27 15:53:23 -0800

    added vimrc and done with install windows

--- install.ps1
+++ install.ps1
@@ -1,5 +1,28 @@
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+
+function RemoveFileIfExst($filePath) {
+    
+    if (Test-Path $filePath) {
+        Remove-Item -Path $filePath 
+    }
+
+}
+
+
+# Vim Settings
+RemoveFileIfExst $env:USERPROFILE\.vimrc
+cmd /c mklink $env:USERPROFILE\.vimrc $dir\.vimrc
 
 # VS Code Settings
-cmd /c mklink \config\Code\User\settings.json %APPDATA%\Roaming\Code\User\settings.json
+RemoveFileIfExst $env:APPDATA\Code\User\settings.json 
+RemoveFileIfExst $env:APPDATA\Code\User\keybindings.json 
 
-cmd /c mklink \config\Code\User\keybindings.json %APPDATA%\Roaming\Code\User\keybindings.json
+cmd /c mklink $env:APPDATA\Code\User\settings.json $dir\config\Code\User\settings.json 
+cmd /c mklink $env:APPDATA\Code\User\keybindings.json $dir\config\Code\User\keybindings.json 
+
+# Helix Editor Settings
+Remove-Item -Path -Path $env:APPDATA\helix\config.toml 
+
+cmd /c mklink $env:APPDATA\helix\config.toml $dir\config\helix\config.toml
+pause
