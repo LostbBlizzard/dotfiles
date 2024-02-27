d8cf8d934bcf08a0afc4eaffd442e6a47b39b5eb
LostbBlizzard <noodproplayz@gmail.com>
2024-02-27 15:11:25 -0800

    added powershell install window

---
+++ install.ps1
@@ -0,0 +1,5 @@
+
+# VS Code Settings
+cmd /c mklink \config\Code\User\settings.json %APPDATA%\Roaming\Code\User\settings.json
+
+cmd /c mklink \config\Code\User\keybindings.json %APPDATA%\Roaming\Code\User\keybindings.json
