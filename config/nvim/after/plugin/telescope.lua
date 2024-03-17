cb4abdb79f458a02c75fff5c16b3ec5ecf5951bc
LostbBlizzard <noodproplayz@gmail.com>
2024-03-17 11:27:35 -0700

    added nvim config

---
+++ config/nvim/after/plugin/telescope.lua
@@ -0,0 +1,12 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n',    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+  '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', function()
+
+builtin.grep_string({ search = vim.fn.input("Grep > ") })	
+
+end)
