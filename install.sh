81ea3870a075e52c9a5549ef6357cdfc5a3053f9
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2024-05-14 16:08:10 -0700

    added install packer for neovim step

--- install.sh
+++ install.sh
@@ -60,6 +60,9 @@ git checkout v0.9.1
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 
+# install packer for neovim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 # neovim
 
 # utills
