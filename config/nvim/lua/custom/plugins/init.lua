5ee17e37798f691f57aa64eb1773f412d5413626
LostbBlizzard <noodproplayz@gmail.com>
2024-06-07 18:03:04 -0700

    updated vifm and lazygit

--- config/nvim/lua/custom/plugins/init.lua
+++ config/nvim/lua/custom/plugins/init.lua
@@ -3,7 +3,8 @@
 --
 -- See the kickstart.nvim README for more information
 vim.api.nvim_set_keymap('n', '<leader>t', ':ToggleTerm<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('t', '<C-\\><C-n>', '<C-\\><C-n>:lua require"toggleterm".close()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('t', '<C-\\><C-n>', '<C-\\><C-n><CR>', {noremap = true})
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>:q<CR><Esc>', {noremap = true})
 
 return {
     {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
