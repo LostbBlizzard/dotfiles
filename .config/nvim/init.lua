e7892a88172b638494ebc108d6eae8e673e2a787
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2024-09-09 17:30:33 -0700

    Remaped open_float

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -128,7 +128,7 @@ vim.api.nvim_command 'autocmd FileType netrw nmap <buffer> r R'
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>k', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 --vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Jump to Netrw' })
