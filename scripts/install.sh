a04b0892e631a838d619dd092597ac3277303687
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2025-11-28 14:06:18 -0500

    fix neovim install

--- scripts/install.sh
+++ scripts/install.sh
@@ -58,7 +58,7 @@ sudo apt-get install ripgrep
 # -applications
 
 # neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz 
+curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz -o nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
